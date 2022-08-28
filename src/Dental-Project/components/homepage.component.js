289f64634681c9c0f28f964851bcdb19b2dd6f4a
coffe789 <jonathan_davey@outlook.com>
2022-08-28 14:14:13 +0800

    Added welcome banner to homepage

--- src/Dental-Project/components/homepage.component.js
+++ src/Dental-Project/components/homepage.component.js
@@ -4,7 +4,10 @@ import { ApplicationProvider, Layout, Text,Card } from '@ui-kitten/components';
 import { Image, StyleSheet, ScrollView, View, SafeAreaView } from 'react-native';
 import { Button } from '@ui-kitten/components';
 import { default as theme } from '../custom-theme.json';
-import tooth from '../assets/tooth(COPYRIGHT).jpg'
+import tooth from '../assets/tooth(COPYRIGHT).jpg';
+import cloud from '../assets/cloud.png';
+import { Dimensions } from 'react-native';
+import { ImageBackground } from 'react-native';
 
 const Header = (props) => (
   <Layout {...props}>
@@ -34,14 +37,25 @@ const Footer = (props) => (
 export const HomeScreen = ({ navigation }) => {
 
   return (
-  <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: "#FFFFF5"}}>
-    <Text category='h1' style={{ color: '#999', fontSize: 40 }}>Welcome home</Text>
+  <Layout style={{flex: 1, alignItems: 'center', backgroundColor: "#FFFFF5"}}>
+    {/* <Text category='h1' style={{ color: '#999', fontSize: 40 }}>Welcome home</Text> */}
+    <View>
+    <Layout style={{paddingBottom:30}}>
+     <ImageBackground source={cloud} resizeMode="cover" style={{width: '100%', height: undefined, aspectRatio: 2400/757}}>
+      <Text level='1'  style={{justifyContent:"center", alignItems:"center", textAlign:'center', lineHeight:100, color:'white', fontSize:30, textShadowColor: "#333333", textShadowOffset: {width:1,height:1}, textShadowRadius: 1}}>Welcome Back!</Text>
+     </ImageBackground>
+    </Layout>
+
+    </View>
     {/* <Layout style={styles.container} level='1'>
       <Button style ={styles.button} status="danger" onPress={() => navigation.navigate('Info')} > Info </Button>
       <Button style ={styles.button} status="success"> Assessment </Button>
       <Button style ={styles.button} status="warning"> Quizzes </Button>
       <Button style ={styles.button} status="info"> Account </Button>
     </Layout> */}
+    <Layout>
+      
+    </Layout>
     <Card style={styles.card} header={Header} footer={Footer}>
       <Text>
         Smoking is bad for your health but you do it anyway because you're stressed depressed and you wonder if its
@@ -69,8 +83,8 @@ const styles = StyleSheet.create({
     flexDirection: 'row'
   },
   card: {
-    flex: 0.3,
-    margin: 2,
+    flex: 0.45,
+    
   },
   footerContainer: {
     flexDirection: 'row',
