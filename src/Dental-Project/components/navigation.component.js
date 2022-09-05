861b92495a07943a7c327434adaa5a0a33695e6d
0123we <97963916+0123we@users.noreply.github.com>
2022-09-05 21:49:34 +0800

    Update navigation.component.js

--- src/Dental-Project/components/navigation.component.js
+++ src/Dental-Project/components/navigation.component.js
@@ -4,12 +4,14 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { HomeScreen } from './homepage.component';
 import { InfoPage } from './infopage.component';
 import { QuizScreen } from './quizpage.component';
+import Assessment from './Assessment';
 
 const { Navigator, Screen } = createStackNavigator();
 
 const HomeNavigator = () => (
   <Navigator screenOptions={{ headerShown: false }}>
     <Screen name='Home' component={HomeScreen} />
+    <Screen name='Assessment' component={Assessment} />
     <Screen name='Info' component={InfoPage} />
     <Screen name='Quiz' component={QuizScreen} />
   </Navigator>
