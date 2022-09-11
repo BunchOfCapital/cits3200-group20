a00e4606f2b0f318076471f8ff7fa5dd7c76dacb
coffe789 <jonathan_davey@outlook.com>
2022-09-11 14:48:57 +0800

    Connect profile to user data

--- src/Dental-Project/components/profile.component.js
+++ src/Dental-Project/components/profile.component.js
@@ -1,18 +1,26 @@
-import React from 'react';
+ import React from 'react';
 import * as eva from '@eva-design/eva';
 import { ApplicationProvider, Layout, Text,Card } from '@ui-kitten/components';
 import { Image, StyleSheet, ScrollView, View, SafeAreaView } from 'react-native';
 import { Button } from '@ui-kitten/components';
 import { default as theme } from '../custom-theme.json';
+import userData from '../Data/userData';
 
 
 export const ProfilePage = () => {
   return (<Layout>
-
+    <Card style={styles.card}>
+      <Text style={{fontSize:18, fontWeight:"500"}}>Profile</Text>
+      <Text>{userData.name + "\n" + userData.email + "\n" + (userData.isChild?"BABY":"BIG AND MANLY")}</Text>
+    </Card>
   </Layout>);
 }
 
 
 const styles = StyleSheet.create({
-
+    card: {
+        borderRadius: 25,
+        elevation: 5,
+        width:"95%"
+    },
 });
