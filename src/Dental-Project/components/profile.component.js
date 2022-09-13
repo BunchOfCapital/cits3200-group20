18d3a41963514829b87cc927abef5d6780162c61
coffe789 <jonathan_davey@outlook.com>
2022-09-13 19:00:22 +0800

    Added profile banner/icon. Fixed initial form visibility

--- src/Dental-Project/components/profile.component.js
+++ src/Dental-Project/components/profile.component.js
@@ -3,7 +3,8 @@ import { Layout, Text } from '@ui-kitten/components';
 import { Image, StyleSheet,KeyboardAvoidingView, ScrollView} from 'react-native';
 import { Button ,Input, Datepicker} from '@ui-kitten/components';
 import userData from '../Data/userData';
-import closeImg from '../assets/DentistImage.jpg'
+import profIcon from '../assets/profileIcon.png'
+import profBanner from '../assets/profileBanner.png'
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 
 
@@ -11,7 +12,7 @@ export const ProfilePage = () =>{
     const [name,setName] = React.useState(userData.name)
     const [email,setEmail] = React.useState(userData.email)
     const [location,setLocation] = React.useState(userData.location)
-    const [boolDisabled,setDisability] = React.useState(false)
+    const [boolDisabled,setDisability] = React.useState(true)
     const [date, setDate] = React.useState(userData.dob);
     const [age, setAge] = React.useState(userData.age)
     const [visibility, setVisible] = React.useState(true)
@@ -46,13 +47,19 @@ export const ProfilePage = () =>{
     const renderButton = () =>{
         if(visibility){
             return(
-            <Button style= {{justifyContent:'center', width: 200, alignSelf:'center', marginTop:40}}status='info' onPress={edittingMode}>Edit Profile</Button>
+            <Button style= {{justifyContent:'center', width: 200, alignSelf:'center', marginTop:40}}status='info' onPress={edittingMode}>
+                Edit Profile
+            </Button>
             )
         }else{
             return(
             <Layout style={{flexDirection:'row',marginTop:40}}>
-                 <Button style= {{justifyContent:'center', width: 150, alignSelf:'center', marginHorizontal:30}}status='danger' onPress={confirmEdit}>Exit</Button>
-                 <Button style= {{justifyContent:'center', width: 150, alignSelf:'center', marginHorizontal:10}}status='success' onPress={cancelEditMode}>Confirm</Button>
+                 <Button style= {{justifyContent:'center', width: 150, alignSelf:'center', marginHorizontal:30}}status='danger' onPress={confirmEdit}>
+                     Exit
+                </Button>
+                 <Button style= {{justifyContent:'center', width: 150, alignSelf:'center', marginHorizontal:10}}status='success' onPress={cancelEditMode}>
+                     Confirm
+                </Button>
             </Layout>
             )
         }
@@ -62,9 +69,10 @@ export const ProfilePage = () =>{
         <Layout style={{flex: 1, backgroundColor: "#FFF",flexDirection:"column"}}>  
             <KeyboardAwareScrollView extraHeight={120}>
             <Layout style ={{alignItems:'center',marginVertical:20}}>
-                <Image source={closeImg} style={{width:100, height:100,borderRadius:200/2}}/>
+                <Image source={profBanner} style={{width:"100%", height:100, borderBottomWidth:3, borderColor:"#000000"}}/>
+                <Image source={profIcon} style={{position: "absolute", width:100, height:100, top:50}}/>
             </Layout >
-            <Layout >
+            <Layout style = {{zIndex: -1}} >
                 <Layout style ={{marginHorizontal:30, backgroundColor:"transparent"}}>
                     <Layout style={styles.container}>
                         <Text>Name: </Text>
