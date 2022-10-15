c70a09491e80e921b740db3bee5b34a720018886
Joel Wildman <joel.l.wildman@gmail.com>
2022-10-15 17:11:54 +0800

    minor style fixes for profile page

--- src/Dental-Project/components/profile.component.js
+++ src/Dental-Project/components/profile.component.js
@@ -109,7 +109,7 @@ export const ProfilePage = () =>{
     const renderButton = () =>{
         if(visibility){
             return(
-            <Button style= {{justifyContent:'center', width: 200, alignSelf:'center', marginTop:40}}status='info' onPress={edittingMode}>
+            <Button style= {{justifyContent:'center', width: 200, alignSelf:'center', marginTop:40, marginBottom: 50}}status='info' onPress={edittingMode}>
                 Edit Profile
             </Button>
             )
@@ -132,7 +132,7 @@ export const ProfilePage = () =>{
     return(
         <Layout style={{flex: 1, backgroundColor: "#FFF",flexDirection:"column"}}>  
             <KeyboardAwareScrollView extraHeight={120}>
-            <Layout style ={{alignItems:'center',marginVertical:20}}>
+            <Layout style ={{alignItems:'center',marginVertical:10}}>
                 <Image source={profBanner} style={{width:"100%", height:100, borderBottomWidth:3, borderColor:"#000000"}}/>
                 <Image source={profIcon} style={{position: "absolute", width:100, height:100, top:50}}/>
             </Layout >
